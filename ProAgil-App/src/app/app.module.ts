649acd0b84a90a5811da325fa26cfeafeea033e9
gfonseca18 <gercia.fonseca@icloud.com>
2020-05-24 13:45:17 +0100

    first commit

---
+++ ProAgil-App/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { from } from 'rxjs';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      EventosComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
